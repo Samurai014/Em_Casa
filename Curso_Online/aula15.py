68f7e5c7ce1d1616b28000f636f3c647089e2c80
samurai <matheusjesus09174@gmail.com>
2023-10-02 12:32:44 -0300

    mundo 3

--- Curso_Online/aula15.py
+++ Curso_Online/aula15.py
@@ -112,3 +112,35 @@
 # print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
 
 #Desafio 71
+# print('='*30)
+# print('{:^30}'.format('Banco CEV'))
+# print('='*30)
+
+# valor = int(input('Que valor você quer sacar? R$'))
+
+# total = valor 
+# cedula = 50
+# totcedula = 0
+
+# while True:
+#     if total >= cedula:
+#         total -= cedula
+#         totcedula += 1
+#     else:
+#         if totcedula > 0:
+#             print(f'Total de {totcedula} cedulas de R${cedula}')
+#         if cedula == 50:
+#             cedula = 20
+#         elif cedula == 20:
+#             cedula = 10
+#         elif cedula == 10:
+#             cedula = 5
+#         elif cedula == 5:
+#             cedula = 1
+        
+#         totcedula = 0
+
+#         if total == 0:
+#             break
+# print('='*30)
+# print('Volte sempre ao Banco CEV! Tenha um bom dia!')
---
+++ Curso_Online/aula16.py
@@ -0,0 +1,12 @@
+# Desafio 72
+
+# Desafio 73
+
+# Desafio 74
+
+# Desafio 75
+
+# Desafio 76
+
+# Desafio 77
+
