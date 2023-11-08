ed31a9f04768bb59649c9e3f397acaa01d702d5f
samurai <matheusjesus09174@gmail.com>
2023-11-08 11:01:22 -0300

    funções em python

--- Curso_Online/aula20.py
+++ Curso_Online/aula20.py
@@ -1,9 +1,66 @@
 # Desafio 96
+# def area(larg, comp):
+#     a = larg * comp
+#     print(f'A área de um terreno {larg}x{comp} é de {a}m².')
+
+# print(' Controle de Terrenos')
+# print('-'*20)
+
+# l = float(input('Largura (m): '))
+# c = float(input('Comprimento (m): '))
+# area(l,c)
 
 # Desafio 97
+# def escreva(msg):
+#     tam = len(msg)+4
+
+#     print('~'*tam)
+#     print(f'  {msg}')
+#     print('~'*tam)
+
+# escreva('Matheus de Jesus Marques')
+# escreva('Estudando Python')
+# escreva('To ficando picaxxs')
 
 # Desafio 98
+# from time import sleep
+# def contador(i, f, p):
+#     if p < 0:
+#         p *= -1
+#     if p == 0:
+#         p = 1
+
+#     print('-='*20)
+#     print(f'Contagem de {i} até {f} em {p} em {p}')
+#     sleep(1.5)
+
+#     if i < f:
+#         cont = i
+#         while cont <= f:
+#             print(f'{cont} ', end='', flush=True)
+#             sleep(0.5)
+#             cont += p
+#         print('FIM')
+#     else:
+#         cont = i
+#         while cont >= f:
+#             print(f'{cont} ', end='', flush=True)
+#             sleep(0.5)
+#             cont -= p
+#         print('FIM')
+
+# contador(1, 10, 1)
+# contador(10, 0, 2)
+# print('-='*20)
+# print('Agora e sua vez de personalizar a contagem!')
+
+# ini = int(input('Inicio: '))
+# fim = int(input('Fim: '))
+# passo = int(input('Passo: '))
+
+# contador(ini, fim, passo)
 
 # Desafio 99
+
 
 # Desafio 100
